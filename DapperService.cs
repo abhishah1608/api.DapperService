6d7f7865f803cffd5c6b660a09fbfb1d82e01a33
abhishah1608 <abhishah1608@gmail.com>
2024-05-15 16:14:33 -0400

    Abhi shah updated functions to satisfy other cases for more complex objects and queries.

--- DapperService.cs
+++ DapperService.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection;
@@ -271,9 +272,28 @@ namespace api.DapperService
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <typeparam name="T4"></typeparam>
+        /// <typeparam name="T5"></typeparam>
+        /// <typeparam name="T6"></typeparam>
+        /// <typeparam name="T7"></typeparam>
+        /// <typeparam name="T8"></typeparam>
+        /// <typeparam name="T9"></typeparam>
+        /// <typeparam name="T10"></typeparam>
+        /// <typeparam name="T11"></typeparam>
+        /// <typeparam name="T12"></typeparam>
+        /// <typeparam name="T13"></typeparam>
+        /// <typeparam name="T14"></typeparam>
+        /// <typeparam name="T15"></typeparam>
+        /// <typeparam name="T16"></typeparam>
+        /// <typeparam name="T17"></typeparam>
+        /// <typeparam name="T18"></typeparam>
+        /// <typeparam name="T19"></typeparam>
+        /// <typeparam name="T20"></typeparam>
         /// <param name="sql"></param>
         /// <param name="parameters"></param>
-        /// <param name="listInfo"></param>
         /// <param name="types"></param>
         /// <returns></returns>
         public async Task<T> QueryMultiple<T, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20>(string sql, DynamicParameters parameters, params Type[] types) where T : new()
@@ -291,90 +311,97 @@ namespace api.DapperService
                 {
                     Type type = types[i];
                     var t1 = (dynamic)null;
+                    PropertyInfo prop = GetPropertyInfo(propertyList, type);
+                    bool singRecord = false;
+                    if (prop == null)
+                    {
+                        singRecord = true;
+                        prop = GetNonListProperty(propertyList, type);
+                    }
+
                     if (typeof(T1) == type)
                     {
-                        t1 = await results.ReadAsync<T1>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T1>() : await results.ReadAsync<T1>();
                     }
                     else if (typeof(T2) == type)
                     {
-                        t1 = await results.ReadAsync<T2>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T2>() : await results.ReadAsync<T2>();
                     }
                     else if (typeof(T3) == type)
                     {
-                        t1 = await results.ReadAsync<T3>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T3>() : await results.ReadAsync<T3>();
                     }
                     else if (typeof(T4) == type)
                     {
-                        t1 = await results.ReadAsync<T4>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T4>() : await results.ReadAsync<T4>();
                     }
                     else if (typeof(T5) == type)
                     {
-                        t1 = await results.ReadAsync<T5>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T5>() : await results.ReadAsync<T5>();
                     }
                     else if (typeof(T6) == type)
                     {
-                        t1 = await results.ReadAsync<T6>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T6>() : await results.ReadAsync<T6>();
                     }
                     else if (typeof(T7) == type)
                     {
-                        t1 = await results.ReadAsync<T7>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T7>() : await results.ReadAsync<T7>();
                     }
                     else if (typeof(T8) == type)
                     {
-                        t1 = await results.ReadAsync<T8>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T8>() : await results.ReadAsync<T8>();
                     }
                     else if (typeof(T9) == type)
                     {
-                        t1 = await results.ReadAsync<T9>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T9>() : await results.ReadAsync<T9>();
                     }
                     else if (typeof(T10) == type)
                     {
-                        t1 = await results.ReadAsync<T10>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T10>() : await results.ReadAsync<T10>();
                     }
                     else if (typeof(T11) == type)
                     {
-                        t1 = await results.ReadAsync<T11>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T11>() : await results.ReadAsync<T11>();
                     }
                     else if (typeof(T12) == type)
                     {
-                        t1 = await results.ReadAsync<T12>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T12>() : await results.ReadAsync<T12>();
                     }
                     else if (typeof(T13) == type)
                     {
-                        t1 = await results.ReadAsync<T13>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T13>() : await results.ReadAsync<T13>();
                     }
                     else if (typeof(T14) == type)
                     {
-                        t1 = await results.ReadAsync<T14>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T14>() : await results.ReadAsync<T14>();
                     }
                     else if (typeof(T15) == type)
                     {
-                        t1 = await results.ReadAsync<T15>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T15>() : await results.ReadAsync<T15>();
                     }
                     else if (typeof(T16) == type)
                     {
-                        t1 = await results.ReadAsync<T16>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T16>() : await results.ReadAsync<T16>();
                     }
                     else if (typeof(T17) == type)
                     {
-                        t1 = await results.ReadAsync<T17>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T17>() : await results.ReadAsync<T17>();
                     }
                     else if (typeof(T18) == type)
                     {
-                        t1 = await results.ReadAsync<T18>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T18>() : await results.ReadAsync<T18>();
                     }
                     else if (typeof(T19) == type)
                     {
-                        t1 = await results.ReadAsync<T19>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T19>() : await results.ReadAsync<T19>();
                     }
                     else if (typeof(T20) == type)
                     {
-                        t1 = await results.ReadAsync<T20>();
+                        t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T20>() : await results.ReadAsync<T20>();
                     }
 
                     if (t1 != null)
-                    {
-                        PropertyInfo prop = GetPropertyInfo(propertyList, type);
+                    {   
                         if (prop != null && prop.CanWrite)
                         {
                             prop.SetValue(t, t1);
@@ -385,6 +412,34 @@ namespace api.DapperService
             return await Task.FromResult(t);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <typeparam name="T4"></typeparam>
+        /// <typeparam name="T5"></typeparam>
+        /// <typeparam name="T6"></typeparam>
+        /// <typeparam name="T7"></typeparam>
+        /// <typeparam name="T8"></typeparam>
+        /// <typeparam name="T9"></typeparam>
+        /// <typeparam name="T10"></typeparam>
+        /// <typeparam name="T11"></typeparam>
+        /// <typeparam name="T12"></typeparam>
+        /// <typeparam name="T13"></typeparam>
+        /// <typeparam name="T14"></typeparam>
+        /// <typeparam name="T15"></typeparam>
+        /// <typeparam name="T16"></typeparam>
+        /// <typeparam name="T17"></typeparam>
+        /// <typeparam name="T18"></typeparam>
+        /// <typeparam name="T19"></typeparam>
+        /// <typeparam name="T20"></typeparam>
+        /// <param name="spname"></param>
+        /// <param name="parameters"></param>
+        /// <param name="types"></param>
+        /// <returns></returns>
         public async Task<T> ExecuteStoredProcedure<T, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20>(string spname, DynamicParameters parameters, params Type[] types) where T : new()
         {
             T t = new T();
@@ -396,98 +451,103 @@ namespace api.DapperService
                 //Set up DynamicParameters object to pass parameters  
                 await connection.OpenAsync();
 
-
                 using (var results = connection.QueryMultiple(spname, parameters, commandType: CommandType.StoredProcedure))
                 {
                     // Anonymous Type
                     for (int i = 0; i < types.Length; i++)
                     {
                         Type type = types[i];
+                        bool singRecord = false;
+                        PropertyInfo prop = GetPropertyInfo(propertyList, type);
+                        if(prop == null)
+                        {
+                            singRecord = true;
+                            prop = GetNonListProperty(propertyList, type);
+                        }
                         var t1 = (dynamic)null;
                         if (typeof(T1) == type)
                         {
-                            t1 = await results.ReadAsync<T1>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T1>() : await results.ReadAsync<T1>();
                         }
                         else if (typeof(T2) == type)
                         {
-                            t1 = await results.ReadAsync<T2>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T2>() : await results.ReadAsync<T2>();
                         }
                         else if (typeof(T3) == type)
                         {
-                            t1 = await results.ReadAsync<T3>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T3>() : await results.ReadAsync<T3>();
                         }
                         else if (typeof(T4) == type)
                         {
-                            t1 = await results.ReadAsync<T4>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T4>() : await results.ReadAsync<T4>();
                         }
                         else if (typeof(T5) == type)
                         {
-                            t1 = await results.ReadAsync<T5>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T5>() : await results.ReadAsync<T5>();
                         }
                         else if (typeof(T6) == type)
                         {
-                            t1 = await results.ReadAsync<T6>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T6>() : await results.ReadAsync<T6>();
                         }
                         else if (typeof(T7) == type)
                         {
-                            t1 = await results.ReadAsync<T7>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T7>() : await results.ReadAsync<T7>();
                         }
                         else if (typeof(T8) == type)
                         {
-                            t1 = await results.ReadAsync<T8>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T8>() : await results.ReadAsync<T8>();
                         }
                         else if (typeof(T9) == type)
                         {
-                            t1 = await results.ReadAsync<T9>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T9>() : await results.ReadAsync<T9>();
                         }
                         else if (typeof(T10) == type)
                         {
-                            t1 = await results.ReadAsync<T10>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T10>() : await results.ReadAsync<T10>();
                         }
                         else if (typeof(T11) == type)
                         {
-                            t1 = await results.ReadAsync<T11>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T11>() : await results.ReadAsync<T11>();
                         }
                         else if (typeof(T12) == type)
                         {
-                            t1 = await results.ReadAsync<T12>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T12>() : await results.ReadAsync<T12>();
                         }
                         else if (typeof(T13) == type)
                         {
-                            t1 = await results.ReadAsync<T13>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T13>() : await results.ReadAsync<T13>();
                         }
                         else if (typeof(T14) == type)
                         {
-                            t1 = await results.ReadAsync<T14>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T14>() : await results.ReadAsync<T14>();
                         }
                         else if (typeof(T15) == type)
                         {
-                            t1 = await results.ReadAsync<T15>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T15>() : await results.ReadAsync<T15>();
                         }
                         else if (typeof(T16) == type)
                         {
-                            t1 = await results.ReadAsync<T16>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T16>() : await results.ReadAsync<T16>();
                         }
                         else if (typeof(T17) == type)
                         {
-                            t1 = await results.ReadAsync<T17>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T17>() : await results.ReadAsync<T17>();
                         }
                         else if (typeof(T18) == type)
                         {
-                            t1 = await results.ReadAsync<T18>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T18>() : await results.ReadAsync<T18>();
                         }
                         else if (typeof(T19) == type)
                         {
-                            t1 = await results.ReadAsync<T19>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T19>() : await results.ReadAsync<T19>();
                         }
                         else if (typeof(T20) == type)
                         {
-                            t1 = await results.ReadAsync<T20>();
+                            t1 = singRecord ? await results.ReadFirstOrDefaultAsync<T20>() : await results.ReadAsync<T20>();
                         }
 
                         if (t1 != null)
                         {
-                            PropertyInfo prop = GetPropertyInfo(propertyList, type);
                             if (prop != null && prop.CanWrite)
                             {
                                 prop.SetValue(t, t1);
@@ -555,10 +615,19 @@ namespace api.DapperService
         /// <returns></returns>
         private PropertyInfo GetPropertyInfo(PropertyInfo[] propertyList, Type type)
         {
-            PropertyInfo info = default;
+            PropertyInfo info = null;
             if (propertyList != null)
             {
                 info = propertyList.Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>)) && p.PropertyType.GetGenericArguments()[0] == type).FirstOrDefault();
+            }
+            return info;
+        }
+
+        private PropertyInfo GetNonListProperty(PropertyInfo[] propertyList, Type type) {
+            PropertyInfo info = null;
+            if (propertyList != null)
+            {
+                info = propertyList.Where(p => !p.PropertyType.IsGenericType && p.PropertyType == type).FirstOrDefault();
             }
             return info;
         }
